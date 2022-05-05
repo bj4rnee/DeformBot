703bafa29004f0bdc082aabb76c21f7168861854
bj4rnee <bjarne.klar@web.de>
2022-05-05 20:53:32 +0200

    download command

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # DeformBot
 An Open Source Image distortion discord bot based on the old version of [@DistortBot](https://twitter.com/DistortBot) on twitter build by [@SergioSV96](https://github.com/SergioSV96)
 
+# Add to your server
+[click here](https://discord.com/oauth2/authorize?client_id=971742838024978463&permissions=140660558912&scope=bot) to add DeformBot to your discord server
+
 # Credits
 [@rupansh](https://github.com/rupansh) for the idea
--- discord/deform_bot.py
+++ discord/deform_bot.py
@@ -1,7 +1,11 @@
 # this is the bot.py
 import os
 import random
+from urllib import request
 import psutil
+import requests
+import uuid
+import shutil
 from datetime import datetime, timedelta
 import discord
 from discord.ext import commands
@@ -11,6 +15,7 @@ from glob import glob
 from PIL import Image
 
 VERSION = "1.0"
+DEBUG = True
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -23,29 +28,33 @@ bot = commands.Bot(command_prefix=COMMAND_PREFIX, help_command=None,
                    description="an Open Source image distortion discord bot")
 client = discord.Client()
 
+embed_error = discord.Embed(description="No attachments", color=0xFF5555)
+embed_error.set_author(name="[Error]", url="https://bjarne.dev/",
+                       icon_url="https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/9e/Barrier_%28held%29_JE2_BE2.png/revision/latest?cb=20200224220440")
+
+
 def distort_image(fname):
     """function to distort an image using the magick library"""
     image = Image.open(fname)
     imgdimens = image.width, image.height
-    distortcmd = f"magick {fname} -liquid-rescale 60x60%! -resize {imgdimens[0]}x{imgdimens[1]}\! result/{fname}"
+    distortcmd = f"magick {fname} -liquid-rescale 60x60%! -resize {imgdimens[0]}x{imgdimens[1]}\! results/{fname}"
 
     os.system(distortcmd)
 
     buf = BytesIO()
     buf.name = 'image.jpeg'
 
-    image = Image.open(f"result/{fname}")
+    image = Image.open(f"results/{fname}")
     filetype = "JPEG" if fname.endswith(".jpg") else "PNG"
     image.save(buf, filetype)
 
     buf.seek(0)
-
     return buf
 
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
     # bot.remove_command('help')
 
 
@@ -71,16 +80,33 @@ async def on_message(message):
     #     await message.channel.send(embed)
 
 
-@bot.command(name='help', help='Shows usage info',aliases=['h', 'info', 'usage'])
+@bot.command(name='help', help='Shows usage info', aliases=['h', 'info', 'usage'])
 async def help(ctx):
     rand_color = random.randint(0, 0xFFFFFF)
-    help_embed = discord.Embed(description="[Website](https://bjarne.dev)\n[Github](https://github.com/bj4rnee/DeformBot)\n\n**Commands:**\n`help`:  Shows this help message\n", color=rand_color)
-    help_embed.set_author(name="Hi, I'm an Open Source image distortion discord bot!", url="https://bjarne.dev/",icon_url="https://cdn.discordapp.com/avatars/971742838024978463/0aa0248616aa2b215640db6b62ad5961.webp?size=80")
+    help_embed = discord.Embed(
+        description="[Website](https://bjarne.dev)\n[Github](https://github.com/bj4rnee/DeformBot)\n\n**Commands:**\n`help`:  Shows this help message\n", color=rand_color)
+    help_embed.set_author(name="Hi, I'm an Open Source image distortion discord bot!", url="https://bjarne.dev/",
+                          icon_url="https://cdn.discordapp.com/avatars/971742838024978463/0aa0248616aa2b215640db6b62ad5961.webp?size=80")
     await ctx.send(embed=help_embed)
 
-@bot.command(name='deform', help='deform an image',aliases=['d', 'distort'])
-async def deform(ctx):
 
-    await ctx.send("empty")
+@bot.command(name='deform', help='deform an image', aliases=['d', 'distort'])
+async def deform(ctx):
+    try:
+        url = ctx.message.attachments[0].url
+    except IndexError:
+        await ctx.send(embed=embed_error)
+        return
+    else:
+        if url[0:26] == "https://cdn.discordapp.com":
+            r = requests.get(url, stream=True)
+            # TODO security check, if file is actually an image!
+            image_name = str(uuid.uuid4()) + '.jpg'
+
+            with open(image_name, 'wb') as out_file:
+                if DEBUG:
+                    print("saving image: "+image_name)
+                shutil.copyfileobj(r.raw, out_file)
+    await ctx.send("done")
 
 bot.run(TOKEN)
