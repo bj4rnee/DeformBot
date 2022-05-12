2b02122978e3ace5f4abbb532c467553fc87f65e
bj4rnee <bjarne.klar@web.de>
2022-05-12 12:40:05 +0200

    added crashdump

--- discord/deform_bot.py
+++ discord/deform_bot.py
@@ -189,17 +189,21 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-    # if message.content == '§help':
-    #     embed = discord.Embed(title="[Debug embed]")
-    #     response = "Hi, I'm an Open Source image distortion discord bot!\n[Website](https://bjarne.dev)\n[Github](https://github.com/bj4rnee/DeformBot)\nCommands:"
-    #     await message.channel.send(embed)
+
+@bot.command(name='crashdump', help='Outputs last stacktrace', aliases=['c', 'cd', 'trace'])
+async def crashdump(ctx):
+    embed_crash = discord.Embed(title=':x: Event Error', color=0xFF5555)
+    #embed_crash.add_field(name='Event', value=event)
+    embed_crash.description = '```py\n%s\n```' % traceback.format_exc()
+    embed_crash.timestamp = datetime.utcnow()
+    await ctx.send(embed=embed_crash)
 
 
 @bot.command(name='help', help='Shows usage info', aliases=['h', 'info', 'usage'])
 async def help(ctx):
     rand_color = random.randint(0, 0xFFFFFF)
-    helpstr_args = "\n\n**Arguments:**\n`l`:  Sean-Carving factor\n`s`:  swirl (degrees)\n`s`:  noise\n`b`:  blur\n`c`:  contrast (allows negative values)\n`o`:  implode\n`d`:  distortion effect\n`i`:  invert colors\n`g`:  grayscale image\n`u`:  disable compression"
-    helpstr_usage = "\n\n**Usage:**\n`§deform [options]`\nExample:  _§deform s35 n95 l45 c+40 b1_"
+    helpstr_args = "\n\n**Arguments:**\n`l`:  Sean-Carving factor\n`s`:  swirl (degrees)\n`s`:  noise\n`b`:  blur\n`c`:  contrast (allows negative values)\n`o`:  implode\n`d`:  distortion effect\n`i`:  invert colors\n`g`:  grayscale image\n`u`:  disable compression\nAll arguments can be arbitrarily combined or left out.\nAll args accept integer values, I advise to play around with those values to find something that looks good."
+    helpstr_usage = "\n\n**Usage:**\n`§deform [options]`\nExamples:\n _§deform s35 n95 l45 c+40 b1_\n_§deform l50 s25 c+30 n70 g_"
     help_embed = discord.Embed(
         description="[Website](https://bjarne.dev)\n[Github](https://github.com/bj4rnee/DeformBot)\n[Twitter](https://twitter.com)\n\n**Commands:**\n`help`:  Shows this help message\n`deform`:  Distort an attached image\nYou can also react to an image with `🤖` to quickly deform it." + helpstr_args + helpstr_usage, color=rand_color)
     help_embed.set_author(name="Hi, I'm an Open Source image distortion discord bot!", url="https://bjarne.dev/",
