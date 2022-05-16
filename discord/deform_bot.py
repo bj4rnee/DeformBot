959760c8ea6fc34a21211d4c3eba2a9557b62741
bj4rnee <bjarne.klar@web.de>
2022-05-16 02:01:23 +0200

    `v1.3` out now!

--- discord/deform_bot.py
+++ discord/deform_bot.py
@@ -11,30 +11,49 @@ import asyncio
 import traceback
 import math
 import gc
+import tweepy
 from numpy import interp
 from argparse import ArgumentParser
 from datetime import datetime, timedelta
 import discord
 from discord.ext import commands, tasks
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key
 from io import BytesIO
 from glob import glob
 from PIL import Image
 from pympler.tracker import SummaryTracker
 from pympler import summary, muppy
 
-VERSION = "1.2.9_dev"
+VERSION = "1.3_dev"
 # Turn off in production!
 DEBUG = True
 
+# load the env variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+CONSUMER_KEY = os.getenv('TWITTER_OAUTH_CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('TWITTER_OAUTH_CONSUMER_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+BEARER_TOKEN = os.getenv('BEARER_TOKEN_MANAGE')
+USER_ID = os.getenv('DB_USER_ID')
+since_id = int(os.getenv('last_id'))
+
+#the bot's command prefix for discord
 COMMAND_PREFIX = '§'
+
 lock = asyncio.Lock()  # Doesn't require event loop
 tracker = SummaryTracker()
 process = psutil.Process(os.getpid())
 start_time = datetime.now()
 arg_error_flag = False
+api = None # twitter api object
+
+# UNFORTUNATELY THIS WORKS ONLY IN v1.1 API
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
 
 bot = commands.Bot(command_prefix=COMMAND_PREFIX, help_command=None,
                    description="an Open Source image distortion discord bot")
@@ -217,11 +236,23 @@ def distort_image(fname, args):
 
 @bot.event
 async def on_ready():
+    global api
     if DEBUG:
         print("──────────────────────────────────────────────────────────────")
         print("starting DeformBot " + VERSION + " ...")
         print(f'{bot.user} has connected to Discord!')
+    await bot.wait_until_ready()
+    # Create API object
+    api_ = tweepy.API(auth, wait_on_rate_limit=True)
+    try:
+        api_.verify_credentials()
+    except Exception as e:
+        raise e
+    if DEBUG:
+        print("[Twitter] Authentication Successful!")
         print("──────────────────────────────────────────────────────────────")
+    api = api_
+    twitter_bot_loop.start()
     # bot.remove_command('help')
 
 
@@ -328,7 +359,7 @@ async def deform(ctx, *args):
 
                         # distort the file
                         distorted_file = distort_image(image_name, args)
-                        
+
                         if arg_error_flag:
                             await ctx.send(embed=argument_error)
 
@@ -413,9 +444,61 @@ async def on_reaction_add(reaction, user):  # if reaction is on a cached message
             return
 
 
+def check_mentions(api, s_id):
+    """check mentions in v1.1 api"""
+    # Retrieving mentions
+    new_since_id = s_id
+    twitter_media_url = ""
+    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=s_id, count=100, tweet_mode='extended').items():
+        new_since_id = max(tweet.id, new_since_id)
+        #os.environ['last_id'] = str(new_since_id)
+        set_key("../discord/.env", 'last_id', str(new_since_id))
+        if hasattr(tweet, 'text'):
+            tweet_txt = tweet.text.lower()
+        else:
+            tweet_txt = ""
+        if hasattr(tweet, 'possibly_sensitive'):
+            sensitive = tweet.possibly_sensitive
+        else:
+            sensitive = False
+        reply_og_id  = tweet.in_reply_to_status_id # original status (if 'tweet' is a reply)
+        if DEBUG:
+            print("[DEBUG] tweet from " + tweet.user.screen_name + ": '" + tweet_txt + "', sensitive: " + str(sensitive) + ", reply: " + str(reply_og_id))
+        
+        if 'media' in tweet.entities: # tweet that mentionions db contains image
+            raw_image = tweet.entities.get('media', [])
+            if(len(raw_image) > 0):
+                twitter_media_url = raw_image[0]['media_url']
+            else:
+                twitter_media_url = "[ERROR] No url found"
+            print(twitter_media_url)
+        else: # tweet that the mentioner replies to contains image
+            if isinstance(reply_og_id, str) or isinstance(reply_og_id, int):
+                r_tweet = api.get_status(reply_og_id)
+                if 'media' in r_tweet.entities:
+                    raw_image = r_tweet.entities.get('media', [])
+                    if(len(raw_image) > 0):
+                        twitter_media_url = raw_image[0]['media_url']
+                    else:
+                        twitter_media_url = "[ERROR] No url found"
+                    print(twitter_media_url)
+                else:
+                    api.update_status(status="[ERROR] no media found.", in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True, possibly_sensitive=sensitive)
+                    continue
+            else:
+                continue
+
+        #result_img = api.media_upload("created_image.png") # TODO 5MB FILESIZE LIMIT!!!!!!!!!!!!!
+
+        api.update_status(status="[DEBUG] fetching image...", in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True, possibly_sensitive=sensitive)
+        #api.update_status('@' + tweet.user.screen_name + " Here's your Quote", tweet.id, media_ids=[result_img.media_id])
+    return new_since_id
+
+
+# THIS IS THE LOOP FOR THE TWITTER BOT
 @tasks.loop(seconds=60)
-async def newloop():
-    print("loop")
-    # loop
+async def twitter_bot_loop():
+    # execute this every minute
+    since_id = check_mentions(api, since_id)
 
 bot.run(TOKEN)
