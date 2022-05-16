239be4cbf673bd6f71501108b9497396307279bc
bj4rnee <bjarne.klar@web.de>
2022-05-16 12:53:36 +0200

    debug args not working

--- discord/deform_bot.py
+++ discord/deform_bot.py
@@ -445,12 +445,12 @@ async def check_mentions(api, s_id):
     """check mentions in v1.1 api"""
     # Retrieving mentions
     new_since_id = s_id
-    print(new_since_id)
     twitter_media_url = ""
     for tweet in tweepy.Cursor(api.mentions_timeline, since_id=new_since_id, count=100, tweet_mode='extended').items():
         new_since_id = max(tweet.id, new_since_id)
         #os.environ['last_id'] = str(new_since_id)
         set_key(".env", 'last_id', str(new_since_id)) # only works when process is terminated
+        print(tweet)
         if hasattr(tweet, 'text'):
             tweet_txt = tweet.text.lower()
         else:
